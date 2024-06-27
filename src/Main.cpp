7b70ab02f88b3f0d856ffe1581f7f567c1a93969
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-06-27 17:40:36 +0900

    Integrating GLFW

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,24 +1,54 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <vulkan/vulkan.h>
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
+
+const uint32_t WIDTH  = 800;
+const uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication
 {
 public:
     void run()
     {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
+    GLFWwindow* window;
+
+    void initWindow()
+    {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
     void initVulkan() {}
 
-    void mainLoop() {}
+    void mainLoop()
+    {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
+    }
 
-    void cleanup() {}
+    void cleanup()
+    {
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
 };
 
 int main()
