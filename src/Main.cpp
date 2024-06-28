8b0c82cd0607f7f333f4fad7419c0d7708cfb6e0
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-06-28 14:19:49 +0900

    Specifying the queues to be created

--- src/Main.cpp
+++ src/Main.cpp
@@ -153,7 +153,18 @@ private:
         return indices;
     }
 
-    void createLogicalDevice() {}
+    void createLogicalDevice()
+    {
+        QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+        VkDeviceQueueCreateInfo queueCreateInfo {};
+        queueCreateInfo.sType            = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+        queueCreateInfo.queueCount       = 1;
+
+        float queuePriority              = 1.0f;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+    }
 
     void createInstance()
     {
