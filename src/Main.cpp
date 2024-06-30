4d9ebc6612bee3350c055c4e9eb62964ec14de02
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-06-30 16:55:02 +0900

    Creating the presentation queue

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <map>
 #include <optional>
+#include <set>
 #include <stdexcept>
 #include <vector>
 
@@ -83,6 +84,8 @@ private:
     VkQueue graphicsQueue;
 
     VkSurfaceKHR surface;
+
+    VkQueue presentQueue;
 
     void initWindow()
     {
@@ -178,20 +181,26 @@ private:
     {
         QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
 
-        VkDeviceQueueCreateInfo queueCreateInfo {};
-        queueCreateInfo.sType            = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
-        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
-        queueCreateInfo.queueCount       = 1;
+        std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+        std::set<uint32_t> uniqueQueueFamilies = {indices.graphicsFamily.value(), indices.presentFamily.value()};
 
-        float queuePriority              = 1.0f;
-        queueCreateInfo.pQueuePriorities = &queuePriority;
+        float queuePriority = 1.0f;
+        for (uint32_t queueFamily : uniqueQueueFamilies)
+        {
+            VkDeviceQueueCreateInfo queueCreateInfo {};
+            queueCreateInfo.sType            = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+            queueCreateInfo.queueCount       = 1;
+            queueCreateInfo.pQueuePriorities = &queuePriority;
+            queueCreateInfos.push_back(queueCreateInfo);
+        }
 
         VkPhysicalDeviceFeatures deviceFeatures {};
 
         VkDeviceCreateInfo createInfo {};
         createInfo.sType                 = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
-        createInfo.pQueueCreateInfos     = &queueCreateInfo;
-        createInfo.queueCreateInfoCount  = 1;
+        createInfo.pQueueCreateInfos     = queueCreateInfos.data();
+        createInfo.queueCreateInfoCount  = static_cast<uint32_t>(queueCreateInfos.size());
         createInfo.pEnabledFeatures      = &deviceFeatures;
         createInfo.enabledExtensionCount = 0;
 
@@ -211,6 +220,7 @@ private:
         }
 
         vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+        vkGetDeviceQueue(device, indices.presentFamily.value(), 0, &presentQueue);
     }
 
     void createInstance()
