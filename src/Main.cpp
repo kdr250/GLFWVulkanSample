a1ea34d37da02437a7f60461669fc797b0fcc867
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-07-01 15:10:45 +0900

    Input assembly

--- src/Main.cpp
+++ src/Main.cpp
@@ -469,6 +469,11 @@ private:
         vertexInputInfo.vertexAttributeDescriptionCount = 0;
         vertexInputInfo.pVertexAttributeDescriptions    = nullptr;  // Optional
 
+        VkPipelineInputAssemblyStateCreateInfo inputAssembly {};
+        inputAssembly.sType                  = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+        inputAssembly.topology               = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+        inputAssembly.primitiveRestartEnable = VK_FALSE;
+
         VkPipelineDynamicStateCreateInfo dynamicState {};
         dynamicState.sType             = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
         dynamicState.dynamicStateCount = static_cast<uint32_t>(dynamicStates.size());
