d186a218824fe6d1d74c178d1c76ffc8533b682f
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-07-01 13:27:03 +0900

    Creating shader modules

--- src/Main.cpp
+++ src/Main.cpp
@@ -442,6 +442,27 @@ private:
     {
         auto vertShaderCode = readFile("resources/shaders/vert.spv");
         auto fragShaderCode = readFile("resources/shaders/frag.spv");
+
+        VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+        VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+        vkDestroyShaderModule(device, fragShaderModule, nullptr);
+        vkDestroyShaderModule(device, vertShaderModule, nullptr);
+    }
+
+    VkShaderModule createShaderModule(const std::vector<char>& code)
+    {
+        VkShaderModuleCreateInfo createInfo {};
+        createInfo.sType    = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
+        createInfo.codeSize = code.size();
+        createInfo.pCode    = reinterpret_cast<const uint32_t*>(code.data());
+
+        VkShaderModule shaderModule;
+        if (vkCreateShaderModule(device, &createInfo, nullptr, &shaderModule) != VK_SUCCESS)
+        {
+            throw std::runtime_error("failed to create shader module!");
+        }
+        return shaderModule;
     }
 
     void createInstance()
