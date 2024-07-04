b66d238dd9034ddd16c3ac8f9d69ef7bed9b1865
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-07-04 11:26:20 +0900

    Basic drawing commands

--- src/Main.cpp
+++ src/Main.cpp
@@ -701,7 +701,25 @@ private:
         renderPassInfo.pClearValues    = &clearColor;
 
         vkCmdBeginRenderPass(commandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
-    }
+
+        vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline);
+
+        VkViewport viewport {};
+        viewport.x        = 0.0f;
+        viewport.y        = 0.0f;
+        viewport.width    = static_cast<float>(swapChainExtent.width);
+        viewport.height   = static_cast<float>(swapChainExtent.height);
+        viewport.minDepth = 0.0f;
+        viewport.maxDepth = 1.0f;
+        vkCmdSetViewport(commandBuffer, 0, 1, &viewport);
+
+        VkRect2D scissor {};
+        scissor.offset = {0, 0};
+        scissor.extent = swapChainExtent;
+        vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
+
+        vkCmdDraw(commandBuffer, 3, 1, 0, 0);
+    };
 
     VkShaderModule createShaderModule(const std::vector<char>& code)
     {
