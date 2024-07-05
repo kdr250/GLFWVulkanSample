98101c8db61cef4a541489cd914a07cacf401973
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-07-05 11:10:04 +0900

    Handling minimization

--- src/Main.cpp
+++ src/Main.cpp
@@ -472,6 +472,14 @@ private:
 
     void recreateSwapChain()
     {
+        int width = 0, height = 0;
+        glfwGetFramebufferSize(window, &width, &height);
+        while (width == 0 || height == 0)
+        {
+            glfwGetFramebufferSize(window, &width, &height);
+            glfwWaitEvents();
+        }
+
         vkDeviceWaitIdle(device);
 
         cleanupSwapChain();
