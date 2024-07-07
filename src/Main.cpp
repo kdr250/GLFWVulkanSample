cc28782c569fe75916f7c68e2962350e3c1666a4
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-07-07 18:36:49 +0900

    Using an index buffer

--- src/Main.cpp
+++ src/Main.cpp
@@ -986,7 +986,9 @@ private:
         VkDeviceSize offsets[]   = {0};
         vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
 
-        vkCmdDraw(commandBuffer, static_cast<uint32_t>(vertices.size()), 1, 0, 0);
+        vkCmdBindIndexBuffer(commandBuffer, indexBuffer, 0, VK_INDEX_TYPE_UINT16);
+
+        vkCmdDrawIndexed(commandBuffer, static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);
 
         vkCmdEndRenderPass(commandBuffer);
 
