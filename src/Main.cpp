b5f574f038c568627ab53b0893887e51c33a3c23
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-07-09 12:53:24 +0900

    Framebuffer

--- src/Main.cpp
+++ src/Main.cpp
@@ -267,9 +267,9 @@ private:
         createRenderPass();
         createDescriptorSetLayout();
         createGraphicsPipeline();
-        createFramebuffers();
         createCommandPool();
         createDepthResources();
+        createFramebuffers();
         createTextureImage();
         createTextureImageView();
         createTextureSampler();
@@ -842,13 +842,13 @@ private:
 
         for (size_t i = 0; i < swapChainImageViews.size(); ++i)
         {
-            VkImageView attachments[] = {swapChainImageViews[i]};
+            std::array<VkImageView, 2> attachments = {swapChainImageViews[i], depthImageView};
 
             VkFramebufferCreateInfo framebufferInfo {};
             framebufferInfo.sType           = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
             framebufferInfo.renderPass      = renderPass;
-            framebufferInfo.attachmentCount = 1;
-            framebufferInfo.pAttachments    = attachments;
+            framebufferInfo.attachmentCount = static_cast<uint32_t>(attachments.size());
+            framebufferInfo.pAttachments    = attachments.data();
             framebufferInfo.width           = swapChainExtent.width;
             framebufferInfo.height          = swapChainExtent.height;
             framebufferInfo.layers          = 1;
