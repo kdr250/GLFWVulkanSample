1ae5a8fbd29a72bee40c447a63c1a409e2e8504d
kdr250 <54400975+kdr250@users.noreply.github.com>
2024-07-10 12:54:57 +0900

    Compute Shader

--- compile.sh
+++ compile.sh
@@ -4,3 +4,4 @@ mypath=$(pwd)
 
 glslc "${mypath}/resources/shaders/shader.vert" -o "${mypath}/resources/shaders/vert.spv"
 glslc "${mypath}/resources/shaders/shader.frag" -o "${mypath}/resources/shaders/frag.spv"
+glslc "${mypath}/resources/shaders/compute.comp" -o "${mypath}/resources/shaders/compute.spv"
